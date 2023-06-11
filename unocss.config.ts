0007352b7d1f9effe3f2d766adc67fd0e99b0897
Niku Grönberg <gronbergniku@gmail.com>
2023-06-11 14:06:21 +0300

    add theme colors, add initial main layout

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,5 +1,26 @@
 import { defineConfig, presetUno } from 'unocss'
+import Color from 'color'
 
 export default defineConfig({
   presets: [presetUno()],
+  theme: {
+    colors: {
+      primary: {
+        dark: Color('#40a02b').darken(0.3).hex(),
+        DEFAULT: Color('#40a02b').hex(),
+      },
+      danger: {
+        dark: Color('#d20f39').darken(0.3).hex(),
+        DEFAULT: Color('#d20f39').hex(),
+      },
+      bg: {
+        DEFAULT: Color('#eff1f5').hex(),
+        surface: Color('#ccd0da').hex(),
+        overlay: Color('#7c7f93').hex(),
+      },
+      text: {
+        DEFAULT: Color('#4c4f69').hex(),
+      },
+    },
+  },
 })
